8c9c58d5f1c1e41973b0ee8ec6b2b704d504d69b
Silvio Gratani <silvio.gratani@gmail.com>
2015-10-01 17:27:28 +0200

    Creata la funzione CheckForSight

---
+++ Source/AI_FromScratch_01/Private/MyBTTask_BlueprintBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AI_FromScratch_01.h"
+#include "MyBTTask_BlueprintBase.h"
+
+
+
+
---
+++ Source/AI_FromScratch_01/Public/MyBTTask_BlueprintBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "BehaviorTree/Tasks/BTTask_BlueprintBase.h"
+#include "MyBTTask_BlueprintBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AI_FROMSCRATCH_01_API UMyBTTask_BlueprintBase : public UBTTask_BlueprintBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
